86a7c16c499e4896698afe3c8a60118fe19cf906
will <williancardoso00004@gmail.com>
2023-05-29 13:59:39 -0400

    funciona

---
+++ db.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+
+let connect = async function () {
+  try {
+    if (global.connection) {
+      return Promise.resolve(global.connection);
+    }
+
+    const pool = new Pool({
+      connectionString: 'postgres://aqadjlqa:S0h0GM6t4yPV3sZQU5i4Kd7B2VHlC7Im@silly.db.elephantsql.com/aqadjlqa'
+    });
+
+    global.connection = pool;
+    return Promise.resolve(pool);
+  } catch (error) {
+    console.error('Erro ao estabelecer a conexão:', error);
+    throw error;
+  }
+};
+
+module.exports = { connect }; 
